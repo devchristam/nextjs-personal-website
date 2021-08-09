ad2b97d45d3da84e48d95fedbb9245d3b03c813d
Chris Tam <tamkachun2908@gmail.com>
2021-08-09 22:23:38 +0800

    feat: add spinner icon

--- pages/components/Spinner.jsx
+++ pages/components/Spinner.jsx
@@ -1,7 +1,9 @@
+import IconSpinner from "./icons/IconSpinner"
 const Spinner = () => {
   return (
     <div className="fixed top-0 right-0 h-screen w-screen z-50 flex justify-center items-center">
-      <div className="animate-spin rounded-full h-24 w-24 border-t-4 border-r-4 border-green-700">
+      <div className="animate-spin h-24 w-24 text-green-700">
+        <IconSpinner />
       </div>
     </div>
   )
